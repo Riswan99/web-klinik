a97becbfe23789dae22be592dbe6dab7ff91b8f9
unknown <ekaangga123456789@gmail.com>
2024-10-08 10:31:28 +0700

    first commit

---
+++ app/Http/Controllers/kotaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class kotaController extends Controller
+{
+    public function index(){
+
+return view('kota.index');
+    }
+}
